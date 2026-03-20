04ec3f29468e32bf7c91e2bbd1584a912fbff04c
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-19 22:49:21 -0500

    feat(util): add ApiConstants class with shared API configuration constants

---
+++ src/test/java/com/turnos/automation/util/ApiConstants.java
@@ -0,0 +1,16 @@
+package com.turnos.automation.util;
+
+public class ApiConstants {
+
+    public static final String BASE_URL_KEY = "restapi.base.url";
+    public static final String DEFAULT_BASE_URL = "http://localhost:3000";
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    public static final String ENDPOINT_SIGN_UP = "/auth/signUp";
+    public static final String ENDPOINT_SIGN_IN = "/auth/signIn";
+    public static final String ENDPOINT_TURNOS = "/turnos";
+}

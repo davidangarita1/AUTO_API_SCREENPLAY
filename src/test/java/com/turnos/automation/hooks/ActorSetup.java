04214f9b329c017b69571e6009aeaa8e3532f917
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-19 22:49:56 -0500

    refactor(hooks): extract ActorSetup from steps and move to dedicated hooks package

--- src/test/java/com/turnos/automation/hooks/ActorSetup.java
+++ src/test/java/com/turnos/automation/hooks/ActorSetup.java
@@ -1,5 +1,6 @@
-package com.turnos.automation.steps;
+package com.turnos.automation.hooks;
 
+import com.turnos.automation.util.ApiConstants;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import net.serenitybdd.screenplay.actors.Cast;
@@ -7,15 +8,12 @@ import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.thucydides.model.environment.SystemEnvironmentVariables;
 
-public class Hooks {
-
-    private static final String BASE_URL_KEY = "restapi.base.url";
-    private static final String DEFAULT_BASE_URL = "http://localhost:3000";
+public class ActorSetup {
 
     @Before
     public void setTheStage() {
         String baseUrl = SystemEnvironmentVariables.createEnvironmentVariables()
-                .getProperty(BASE_URL_KEY, DEFAULT_BASE_URL);
+                .getProperty(ApiConstants.BASE_URL_KEY, ApiConstants.DEFAULT_BASE_URL);
         OnStage.setTheStage(Cast.whereEveryoneCan(CallAnApi.at(baseUrl)));
         OnStage.theActorCalled("testUser");
     }
